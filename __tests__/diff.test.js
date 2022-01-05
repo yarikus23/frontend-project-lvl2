eea641f910c969f1ea264746453bd494da9599c1
Yaroslav <yarikus23@gmail.com>
2022-01-05 21:33:02 +0200

    modified diff.test.js - first test

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -3,16 +3,18 @@ import { readFileSync } from 'fs';
 import path, { dirname } from 'path';
 import genDiff from '../src/diff.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 test('equal keys, equal values', () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  const result = '{host: hexlet.io}';
+  const etalon = '{\n    host: hexlet.io\n}';
+  // const etalonFilePath = path.join(__dirname, '..', '__fixtures__', 'eq-eq_etalon.json');
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq1.json');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq2.json');
+  // const etalonFile = `${readFileSync(etalonFilePath, 'utf8')}`;
   const file1 = readFileSync(file1Path, 'utf8');
   const file2 = readFileSync(file2Path, 'utf8');
-  console.log(file1Path);
-  expect(genDiff(file1, file2)).toEqual(result);
+  expect(genDiff(file1, file2)).toEqual(etalon);
 });
 
 // "echo \"Error: no test specified\" && exit 1";
