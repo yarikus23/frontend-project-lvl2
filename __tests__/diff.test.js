fdfc7563447bc6f25d1b5bdc1952766a94d584fe
Yaroslav <yarikus23@gmail.com>
2022-01-05 20:47:25 +0200

    first modified diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,14 +1,18 @@
-import fileURLToPath from 'url';
-import { dirname, path } from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import path, { dirname } from 'path';
 import genDiff from '../src/diff.js';
 
 test('equal keys, equal values', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
-  const result = { host: 'hexlet.io' };
+  const result = '{host: hexlet.io}';
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq1.json');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq2.json');
-  expect(genDiff(file1Path, file2Path)).toEqual(result);
+  const file1 = readFileSync(file1Path, 'utf8');
+  const file2 = readFileSync(file2Path, 'utf8');
+  console.log(file1Path);
+  expect(genDiff(file1, file2)).toEqual(result);
 });
 
 // "echo \"Error: no test specified\" && exit 1";
