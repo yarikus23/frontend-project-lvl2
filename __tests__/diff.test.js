e78294cea2f05f02eec3db8dbddea65e5d2faef6
Yaroslav <yarikus23@gmail.com>
2022-01-08 16:29:45 +0200

    added noteq-noteq files and modified diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -8,13 +8,27 @@ const __dirname = dirname(__filename);
 
 test('equal keys, equal values', () => {
   const etalon = '{\n    host: hexlet.io\n}';
-  // const etalonFilePath = path.join(__dirname, '..', '__fixtures__', 'eq-eq_etalon.json');
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq1.json');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq2.json');
-  // const etalonFile = `${readFileSync(etalonFilePath, 'utf8')}`;
   const file1 = readFileSync(file1Path, 'utf8');
   const file2 = readFileSync(file2Path, 'utf8');
   expect(genDiff(file1, file2)).toEqual(etalon);
 });
 
-// "echo \"Error: no test specified\" && exit 1";
+test('equal keys, not equal values', () => {
+  const etalon = '{\n  - host: hexlet.io\n  + host: google.com\n}';
+  const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-noteq1.json');
+  const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-noteq2.json');
+  const file1 = readFileSync(file1Path, 'utf8');
+  const file2 = readFileSync(file2Path, 'utf8');
+  expect(genDiff(file1, file2)).toEqual(etalon);
+});
+
+test('not equal keys, not equal values', () => {
+  const etalon = '{\n  + browser: google\n  - host: hexlet.io\n}';
+  const file1Path = path.join(__dirname, '..', '__fixtures__', 'noteq-noteq1.json');
+  const file2Path = path.join(__dirname, '..', '__fixtures__', 'noteq-noteq2.json');
+  const file1 = readFileSync(file1Path, 'utf8');
+  const file2 = readFileSync(file2Path, 'utf8');
+  expect(genDiff(file1, file2)).toEqual(etalon);
+});
