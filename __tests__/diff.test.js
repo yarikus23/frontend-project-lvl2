2241a21f3a384828e6023ea8e5c9a6b9872ec317
Yaroslav <yarikus23@gmail.com>
2022-01-29 20:58:29 +0200

    modified diff.test.js

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,34 +1,45 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/diff.js';
 import parser from '../src/parsers.js';
+import toStylish from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const pathBuilder = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
-test('equal keys, equal values', () => {
-  const etalon = '{\n    host: hexlet.io\n}';
-  const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq1.json');
-  const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-eq2.json');
-  const obj1 = parser(file1Path);
-  const obj2 = parser(file2Path);
-  expect(genDiff(obj1, obj2)).toEqual(etalon);
+test('removing and adding values', () => {
+  const obj1 = parser(pathBuilder('remove.json'));
+  const obj2 = parser(pathBuilder('add.json'));
+  const etalon = readFileSync(pathBuilder('remove-add.yaml'), 'utf8');
+  expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
-test('equal keys, not equal values', () => {
-  const etalon = '{\n  - host: hexlet.io\n  + host: google.com\n}';
-  const file1Path = path.join(__dirname, '..', '__fixtures__', 'eq-noteq1.json');
-  const file2Path = path.join(__dirname, '..', '__fixtures__', 'eq-noteq2.json');
-  const obj1 = parser(file1Path);
-  const obj2 = parser(file2Path);
-  expect(genDiff(obj1, obj2)).toEqual(etalon);
+test('updating values', () => {
+  const obj1 = parser(pathBuilder('updated1.json'));
+  const obj2 = parser(pathBuilder('updated2.json'));
+  const etalon = readFileSync(pathBuilder('updated.yaml'), 'utf8');
+  expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
-test('not equal keys, not equal values', () => {
-  const etalon = '{\n  + browser: google\n  - host: hexlet.io\n}';
-  const file1Path = path.join(__dirname, '..', '__fixtures__', 'noteq-noteq1.json');
-  const file2Path = path.join(__dirname, '..', '__fixtures__', 'noteq-noteq2.json');
-  const obj1 = parser(file1Path);
-  const obj2 = parser(file2Path);
-  expect(genDiff(obj1, obj2)).toEqual(etalon);
+test('without changes', () => {
+  const obj1 = parser(pathBuilder('noChange1.json'));
+  const obj2 = parser(pathBuilder('noChange2.json'));
+  const etalon = readFileSync(pathBuilder('noChange.yaml'), 'utf8');
+  expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
+});
+
+test('big data JSON', () => {
+  const obj1 = parser(pathBuilder('deepFile1.json'));
+  const obj2 = parser(pathBuilder('deepFile2.json'));
+  const etalon = readFileSync(pathBuilder('etalon.yaml'), 'utf8');
+  expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
+});
+
+test('big data YAML', () => {
+  const obj1 = parser(pathBuilder('deepFile1.yaml'));
+  const obj2 = parser(pathBuilder('deepFile2.yaml'));
+  const etalon = readFileSync(pathBuilder('etalon.yaml'), 'utf8');
+  expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
