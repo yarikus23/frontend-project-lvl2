697e6698ae095824bd7f789d6a5d84058cc94ba6
Yaroslav <yarikus23@gmail.com>
2022-02-01 21:23:14 +0200

    optimize stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -1,37 +1,32 @@
-const toStylish = (data, replacer = ' ', spacesCount = 2) => {
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object') {
-      return currentValue.toString();
+const toStylish = (data, depth = 1) => {
+  if (typeof data !== 'object') {
+    return data.toString();
+  }
+  if (data === null) {
+    return data;
+  }
+  const indentSize = depth * 2;
+  const currentIndent = ' '.repeat(indentSize);
+  const bracketIndent = ' '.repeat(indentSize - 2);
+  const lines = Object.entries(data).map(([key, val]) => {
+    let bar = '  ';
+    let value = val.val1;
+    if (val.type === 'deleted') {
+      bar = '- ';
     }
-    if (currentValue === null) {
-      return currentValue;
+    if (val.type === 'added') {
+      bar = '+ ';
+      value = val.val2;
     }
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => {
-        let bar = '  ';
-        let value = val.val1;
-        if (val.type === 'deleted') {
-          bar = '- ';
-        }
-        if (val.type === 'added') {
-          bar = '+ ';
-          value = val.val2;
-        }
-        if (val.type === undefined) {
-          value = val;
-        }
-        if (val.type === 'changed') {
-          return `${currentIndent}- ${key}: ${iter(val.val1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.val2, depth + 2)}`;
-        }
-        return `${currentIndent}${bar}${key}: ${iter(value, depth + 2)}`;
-      });
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
-  };
-  return iter(data, 1);
+    if (val.type === undefined) {
+      value = val;
+    }
+    if (val.type === 'changed') {
+      return `${currentIndent}- ${key}: ${toStylish(val.val1, depth + 2)}\n${currentIndent}+ ${key}: ${toStylish(val.val2, depth + 2)}`;
+    }
+    return `${currentIndent}${bar}${key}: ${toStylish(value, depth + 2)}`;
+  });
+  return ['{', ...lines, `${bracketIndent}}`].join('\n');
 };
 
 export default toStylish;
