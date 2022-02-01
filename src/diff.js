6cc668331746f952beac357038a7c484bf2ace50
Yaroslav <yarikus23@gmail.com>
2022-02-01 19:51:40 +0200

    optimize diff.js

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,7 @@
 import _ from 'lodash';
 
 const genDiff = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const allKeys = _.union(keys1, keys2);
+  const allKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   const sortedKeys = _.sortBy(allKeys);
   const diff = sortedKeys.reduce((acc, key) => {
     const value1 = obj1[key];
@@ -18,15 +16,13 @@ const genDiff = (obj1, obj2) => {
         currentVal = { type: 'unchanged', val1: value1 };
       } else {
         currentVal = { type: 'changed', val1: value1, val2: value2 };
-      }
-      return { ...acc, [key]: currentVal };
+      } return { ...acc, [key]: currentVal };
     }
     if (_.has(obj1, key)) {
       currentVal = { type: 'deleted', val1: value1 };
     } else {
       currentVal = { type: 'added', val2: value2 };
-    }
-    return { ...acc, [key]: currentVal };
+    } return { ...acc, [key]: currentVal };
   }, {});
   return diff;
 };
