bffe87468e2bcafc5ed4f7df6c41ba612f9dc00b
Yaroslav <yarikus23@gmail.com>
2022-02-12 23:38:43 +0200

    modified bin/info.js

--- bin/info.js
+++ bin/info.js
@@ -11,15 +11,13 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    let formatted;
     if (options.format === 'stylish') {
-      formatted = genDiff(filepath1, filepath2);
+      console.log(genDiff(filepath1, filepath2));
     } else if (options.format === 'plain') {
-      formatted = genDiff(filepath1, filepath2, 'plain');
+      console.log(genDiff(filepath1, filepath2, 'plain'));
     } else if (options.format === 'json') {
-      formatted = genDiff(filepath1, filepath2, 'json');
+      console.log(genDiff(filepath1, filepath2, 'json'));
     }
-    console.log(formatted);
   });
 
 program.parse();
