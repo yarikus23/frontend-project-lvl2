3d766de9cf7fcd119c45fb36d1f8d8c36d862c89
Yaroslav <yarikus23@gmail.com>
2022-02-12 12:52:11 +0200

    modified pars name in tests

--- __tests__/plain.test.js
+++ __tests__/plain.test.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import genDiff from '../src/diff.js';
-import parser from '../src/parsers.js';
+import parse from '../src/parsers.js';
 import plainer from '../src/formatters/plain.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,29 +9,29 @@ const __dirname = dirname(__filename);
 const pathBuilder = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
 test('adding values', () => {
-  const obj1 = parser(pathBuilder('plain-add1.json'));
-  const obj2 = parser(pathBuilder('plain-add2.json'));
+  const obj1 = parse(pathBuilder('plain-add1.json'));
+  const obj2 = parse(pathBuilder('plain-add2.json'));
   const etalon = "Property 'setting4' was added with value: 'blah blah'";
   expect(plainer(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('removing values', () => {
-  const obj1 = parser(pathBuilder('plain-remove1.json'));
-  const obj2 = parser(pathBuilder('plain-remove2.json'));
+  const obj1 = parse(pathBuilder('plain-remove1.json'));
+  const obj2 = parse(pathBuilder('plain-remove2.json'));
   const etalon = "Property 'setting3' was removed";
   expect(plainer(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('updating values', () => {
-  const obj1 = parser(pathBuilder('plain-update1.json'));
-  const obj2 = parser(pathBuilder('plain-update2.json'));
+  const obj1 = parse(pathBuilder('plain-update1.json'));
+  const obj2 = parse(pathBuilder('plain-update2.json'));
   const etalon = "Property 'nest' was updated. From [complex value] to null";
   expect(plainer(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('unchange and path of keys', () => {
-  const obj1 = parser(pathBuilder('plain-unchange1.json'));
-  const obj2 = parser(pathBuilder('plain-unchange2.json'));
+  const obj1 = parse(pathBuilder('plain-unchange1.json'));
+  const obj2 = parse(pathBuilder('plain-unchange2.json'));
   const etalon = "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'";
   expect(plainer(genDiff(obj1, obj2))).toEqual(etalon);
 });
--- __tests__/stylish.test.js
+++ __tests__/stylish.test.js
@@ -2,44 +2,44 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
 import genDiff from '../src/diff.js';
-import parser from '../src/parsers.js';
-import toStylish from '../src/stylish.js';
+import parse from '../src/parsers.js';
+import toStylish from '../src/formatters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const pathBuilder = (name) => path.join(__dirname, '..', '__fixtures__', name);
 
 test('removing and adding values', () => {
-  const obj1 = parser(pathBuilder('remove.json'));
-  const obj2 = parser(pathBuilder('add.json'));
+  const obj1 = parse(pathBuilder('remove.json'));
+  const obj2 = parse(pathBuilder('add.json'));
   const etalon = readFileSync(pathBuilder('remove-add.yaml'), 'utf8');
   expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('updating values', () => {
-  const obj1 = parser(pathBuilder('updated1.json'));
-  const obj2 = parser(pathBuilder('updated2.json'));
+  const obj1 = parse(pathBuilder('updated1.json'));
+  const obj2 = parse(pathBuilder('updated2.json'));
   const etalon = readFileSync(pathBuilder('updated.yaml'), 'utf8');
   expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('without changes', () => {
-  const obj1 = parser(pathBuilder('noChange1.json'));
-  const obj2 = parser(pathBuilder('noChange2.json'));
+  const obj1 = parse(pathBuilder('noChange1.json'));
+  const obj2 = parse(pathBuilder('noChange2.json'));
   const etalon = readFileSync(pathBuilder('noChange.yaml'), 'utf8');
   expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('big data JSON', () => {
-  const obj1 = parser(pathBuilder('deepFile1.json'));
-  const obj2 = parser(pathBuilder('deepFile2.json'));
+  const obj1 = parse(pathBuilder('deepFile1.json'));
+  const obj2 = parse(pathBuilder('deepFile2.json'));
   const etalon = readFileSync(pathBuilder('etalon.yaml'), 'utf8');
   expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
 
 test('big data YAML', () => {
-  const obj1 = parser(pathBuilder('deepFile1.yaml'));
-  const obj2 = parser(pathBuilder('deepFile2.yaml'));
+  const obj1 = parse(pathBuilder('deepFile1.yaml'));
+  const obj2 = parse(pathBuilder('deepFile2.yaml'));
   const etalon = readFileSync(pathBuilder('etalon.yaml'), 'utf8');
   expect(toStylish(genDiff(obj1, obj2))).toEqual(etalon);
 });
