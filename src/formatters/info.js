4fdf3f11cfbd23b6cead6d6f39f1f55dfbdcd578
Yaroslav <yarikus23@gmail.com>
2022-02-13 21:58:50 +0200

    modified plain.js

--- src/formatters/info.js
+++ src/formatters/info.js
@@ -3,7 +3,6 @@ import plain from './plain.js';
 import toJson from './json.js';
 
 const formatter = (diff, formatName) => {
-  // let format = stylish(diff);
   if (formatName === 'plain') {
     return plain(diff);
   }
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 const toPlain = (data, parent = []) => {
   const lines = Object.entries(data).reduce((acc, [key, val]) => {
     let value1 = '[complex value]';
@@ -9,14 +11,13 @@ const toPlain = (data, parent = []) => {
       value2 = (typeof val.val2 === 'string') ? `'${val.val2}'` : val.val2;
     }
     const keyPath = [...parent, key].join('.');
-    if (val.type === 'deleted') {
-      acc.push(`Property '${keyPath}' was removed`);
-    }
-    if (val.type === 'added') {
-      acc.push(`Property '${keyPath}' was added with value: ${value1}`);
-    }
-    if (val.type === 'changed') {
-      acc.push(`Property '${keyPath}' was updated. From ${value1} to ${value2}`);
+    const values = {
+      deleted: `Property '${keyPath}' was removed`,
+      added: `Property '${keyPath}' was added with value: ${value1}`,
+      changed: `Property '${keyPath}' was updated. From ${value1} to ${value2}`,
+    };
+    if (_.has(values, val.type)) {
+      return [...acc, values[val.type]];
     }
     if (val.type === 'unchanged' && typeof val.val1 === 'object' && val.val1 !== null) {
       return [...acc, toPlain(val.val1, [...parent, key])];
